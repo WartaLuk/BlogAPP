3c937e52b009030444965ed2f704d0710c9f870e
Łukasz Wartałowicz <wartalowiczl@gmail.com>
2022-08-20 22:38:44 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+'use strict'
+import {Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <main>
+      "Hello World"
+    </main>
+  )
+}
+export default App;
---
+++ src/redux/initialState.js
@@ -0,0 +1,20 @@
+const initialState = {
+    posts: [
+        {
+            id: "1",
+            title: "Nutrition of women with hair loss problem during the period of menopause",
+            author: "Zuzanna Sabina Goluch-Koniuszy",
+            text: "DSADASDSADSDADDDADSDSADSADAASDADSADS"
+
+        },
+        {
+            id: "2",
+            title: "PHISIC-CHEMICAL CHARACTERIZATION AND ANT-OXIDANT PRESENT IN THE DEGREASED FLOUR OF BAURU ",
+            author: "Ana Maria Costa & Herbert Cavalcante de Lima & Lívia de Lacerda de Oliveira Pineli & Sonia & Maria Costa Celestino",
+            text: "RTRESSTERTRETETETERTERTERTETRETERTERTEERTE"
+        },
+    ],
+
+};
+
+export default initialState;
