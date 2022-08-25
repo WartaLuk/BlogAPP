9502300945d0fad7c88ea9b5dc8e0f596950dc21
Łukasz Wartałowicz <wartalowiczl@gmail.com>
2022-08-25 18:20:46 +0200

    Test remove-post function

--- src/components/pages/Post/Post.js
+++ src/components/pages/Post/Post.js
@@ -1,27 +1,51 @@
-import {Card} from "react-bootstrap";
-import { useSelector } from "react-redux";
-import { getPostById } from "../../../redux/postsRedux";
-import {useParams, Navigate} from "react-router";
-
+import { Card, Button, Modal } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { getPostById, removePost } from "../../../redux/postsRedux";
+import { useParams, Navigate } from "react-router";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Post = () => {
-    const {postId} = useParams();
-    const post = useSelector((state) => getPostById(state, postId));
+  const { postId } = useParams();
+  const post = useSelector((state) => getPostById(state, postId));
+  const [show, setShow] = useState(false);
+  const dispatch = useDispatch();
 
-if (!post) return <Navigate to="/" />;
-else
-  return (
-    <section>
-            <Card.Body>  
-              <Card.Title>
-                <h5>Title: {post.title}</h5>
-              </Card.Title>
-              <Card.Subtitle>
-                <h6>Author: {post.author}</h6>
-              </Card.Subtitle>
-              <Card.Text>{post.text}</Card.Text>
-            </Card.Body>
-    </section>
-  );
+  const handleRemove = (e) => {
+    e.preventDefault();
+    dispatch(removePost(post.id));
+  };
+
+  if (!post) return <Navigate to="/" />;
+  else
+    return (
+      <section>
+        <Card.Body>
+          <Card.Title>
+            <h5>Title: {post.title}</h5>
+          </Card.Title>
+          <Card.Subtitle>
+            <h6>Author: {post.author}</h6>
+          </Card.Subtitle>
+          <Card.Text>{post.text}</Card.Text>
+        </Card.Body>
+        <Button className="m-3" as={Link} to={`/post/edit/${postId}`}>Edit</Button>
+        <Button className="m-3" onClick={() => setShow(true)}>Delete</Button>
+        <Modal show={show} onHide={() => setShow(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Are you sure?</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>This operation will pernamently remove this article.<br/> Are you sure you want to do that?</Modal.Body>
+          <Modal.Footer className="p-3">
+            <Button variant="secondary" onClick={() => setShow(false)}>
+              Cancel
+            </Button>
+            <Button variant="primary" onClick={() => setShow(true)}>
+             Remove
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </section>
+    );
 };
 export default Post;
--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -6,11 +6,20 @@ export const getPostById  =({posts}, postId) => posts.find((post) => post.id ===
 // actions
 const createActionName = actionName => `app/posts/${actionName}`;
 const ADD_POST = createActionName('ADD_POST');
+const REMOVE_POST = createActionName('REMOVE_POST');
 // action creators
+export const removePost = (payload) => ({ type: REMOVE_POST, payload });
+
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
     case ADD_POST:
       return [...statePart, {...action.payload}];
+    case REMOVE_POST:
+      return {
+        state,
+        posts: state.posts.filter((post) => post.id !== action.payload),
+      };
+      // [...
     default:
       return statePart;
   };
