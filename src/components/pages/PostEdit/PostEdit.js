fb2a5564d54987e622501d0e70990c5725823e64
Łukasz Wartałowicz <wartalowiczl@gmail.com>
2022-09-01 19:33:12 +0200

    Add EditPost

--- src/components/pages/PostEdit/PostEdit.js
+++ src/components/pages/PostEdit/PostEdit.js
@@ -1,6 +1,13 @@
+import PostEditForm from "../../features/PostEditForm/PostEditForm";
+import { useParams } from "react-router-dom";
+import { getPostById } from "../../../redux/postsRedux";
+import { useSelector } from "react-redux";
+
 const PostEdit = () => {
+  const {postId} = useParams();
+  const post = useSelector(state => getPostById(state, postId));
     return (
-        <h1>PostEdit</h1>
+        <PostEditForm post={post}/>
     );
 };
 
